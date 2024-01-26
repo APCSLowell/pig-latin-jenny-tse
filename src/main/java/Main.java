39c6a3594f1da7ffecc33673a84e674f87e36447
Jenny Tse <142839347+jenny-tse@users.noreply.github.com>
2024-01-26 12:36:43 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,18 @@
-
 public void setup() 
 {
-  String[] lines = {"beast","dough","happy","question","star","three","eagle","try"};
-  for (int i = 0 ; i < lines.length; i++) 
-  {
-    System.out.println(pigLatin(lines[i]));
-  }
+	String[] lines = loadStrings("words.txt");
+	System.out.println("there are " + lines.length + " lines");
+	for (int i = 0 ; i < lines.length; i++) 
+	{
+	  System.out.println(pigLatin(lines[i]));
+	}
 }
 public void draw()
 {
         //not used
 }
-public int findFirstVowel(String sWord){
-  if(sWord.length()>0){
+public int findFirstVowel(String sWord)
+if(sWord.length()>0){
     for(int i=0;i<sWord.length();i++){
       if(sWord.substring(i,i+1).equals("a")||sWord.substring(i,i+1).equals("e")||sWord.substring(i,i+1).equals("i")||sWord.substring(i,i+1).equals("o")||sWord.substring(i,i+1).equals("u")){
         return i;
@@ -20,11 +20,9 @@ public int findFirstVowel(String sWord){
     }
   }
   return -1;
-  
 }
-public String pigLatin(String sWord){
-//precondition: sWord is a valid String of length greater than 0
-//postcondition: returns the pig latin equivalent of sWord
+
+public String pigLatin(String sWord)
 if(findFirstVowel(sWord)==1&&sWord.substring(0,1).equals("q")){
     return sWord.substring(2)+"quay";
   }
@@ -46,4 +44,3 @@ else
     return "ERROR!";
   }
 }
-
